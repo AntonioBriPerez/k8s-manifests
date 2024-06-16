8586d89e9dc7a04297d75ba8b2abc01443966cf1
AntonioBriPerez <antoniobirrin@gmail.com>
2024-06-16 18:17:34 +0200

    saved important manifests

--- services/argo-cd/configure.sh
+++ services/argo-cd/configure.sh
@@ -7,6 +7,8 @@ if [ "$action" == "i" ]; then
     kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
     kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
     kubectl get svc -n argocd
+    echo "La master password es: "
+    kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
     echo "ArgoCD installed"
 elif [ "$action" == "u" ]; then
     echo "Uninstalling ArgoCD"
