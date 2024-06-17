92d919f5d19f19edd88b0a9fd49958dac32900f5
AntonioBriPerez <antoniobirrin@gmail.com>
2024-06-17 21:10:33 +0200

    saved important manifests

--- services/grafana-prometheus/configure.sh
+++ services/grafana-prometheus/configure.sh
@@ -3,4 +3,5 @@
  kubectl create namespace monitoring
  helm install prometheus bitnami/kube-prometheus --namespace monitoring
  helm install grafana bitnami/grafana --values grafana/my_values_grafana.yml --namespace monitoring
+ sleep 30
  echo "$(kubectl get secret grafana-admin --namespace default -o jsonpath="{.data.GF_SECURITY_ADMIN_PASSWORD}" | base64 --decode)"
