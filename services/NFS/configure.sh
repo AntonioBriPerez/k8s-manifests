a38c83d04485d3246b21acad46d376680d801f52
AntonioBriPerez <antoniobirrin@gmail.com>
2024-06-17 17:12:11 +0200

    saved important manifests

--- services/NFS/configure.sh
+++ services/NFS/configure.sh
@@ -2,7 +2,7 @@
 echo "Wanna install or uninstall? (i/u)"
 read option
 if [ $option == "i" ]; then
-    echo "Introduce la IP del controlplane:"
+    echo "Introduce la IP del controlplane NO INTRODUCIR DNS:"
     read controlplane_ip
 
     echo "Introduce IP del nodo 1:"
@@ -14,9 +14,9 @@ if [ $option == "i" ]; then
     echo "Introduce el nombre de usuario de la maquina"
     read username
 
-    ssh -i ./private_key_m $username@$controlplane_ip 'bash -s' -- $node1_ip < ./install_nfs_server.sh
-    ssh -i ./private_key_n1 $username@$node1_ip 'bash -s' -- $node1_ip < ./install_nfs_client.sh
-    ssh -i ./private_key_n2 $username@$node2_ip 'bash -s' -- $node1_ip < ./install_nfs_client.sh
+    ssh -i ../../ssh_keys/private_key_cp $username@$controlplane_ip 'bash -s' -- $node1_ip < ./install_nfs_server.sh
+    ssh -i ../../ssh_keys/private_key_n1 $username@$node1_ip 'bash -s' -- $node1_ip < ./install_nfs_client.sh
+    ssh -i ../../ssh_keys/private_key_n2 $username@$node2_ip 'bash -s' -- $node1_ip < ./install_nfs_client.sh
     kubectl create namespace nfs
     helm repo add nfs-subdir-external-provisioner https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner
     helm upgrade --install nfs-subdir-external-provisioner \
